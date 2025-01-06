a5475d5ebfc4f4ed343b87116597ffb797bab122
도훈 <modooboiroo@gmail.com>
2025-01-06 10:48:39 +0900

    first commit

---
+++ src/main/kotlin/org/example/root_be/RootBeApplication.kt
@@ -0,0 +1,11 @@
+package org.example.root_be
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RootBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<RootBeApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/root_be/RootBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.root_be
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RootBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
