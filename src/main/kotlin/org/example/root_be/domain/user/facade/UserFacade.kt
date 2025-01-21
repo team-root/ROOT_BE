34aaf6639329f7948cd5ca94798abcbbc34bf329
in-jun <me@injun.dev>
2025-01-22 08:13:42 +0900

    feat: (#5) SecurityContextHolder를 이용한 현재 유저 조회 기능 추가
    SecurityContextHolder를 사용하여 현재 로그인한 유저의 정보를
    가져올 수 있도록 getCurrentUser() 메서드를 추가함
    
    - 기존에는 유저 정보를 가져오려면 매번 Repository를 통해
      직접 조회해야 했음
    - 이제 getCurrentUser() 메서드를 통해 간편하게 현재
      로그인한 유저의 정보를 얻을 수 있음
    - UserFacade에서 유저 정보 접근 로직을 일원화하여 코드
      중복을 줄임

--- src/main/kotlin/org/example/root_be/domain/user/facade/UserFacade.kt
+++ src/main/kotlin/org/example/root_be/domain/user/facade/UserFacade.kt
@@ -3,6 +3,7 @@ package org.example.root_be.domain.user.facade
 import org.example.root_be.domain.user.domain.User
 import org.example.root_be.domain.user.domain.repository.UserRepository
 import org.example.root_be.domain.user.exception.UserNotFoundException
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 
 @Component
@@ -13,4 +14,8 @@ class UserFacade(
         userRepository.findById(id)
             .orElseThrow { throw UserNotFoundException() }
 
+    fun getCurrentUser(): User {
+        val id = SecurityContextHolder.getContext().authentication.name
+        return getUserById(id.toLong())
+    }
 }
