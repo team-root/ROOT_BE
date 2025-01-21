2ba463c862f8cb93cbb08ba62b572d702a414df3
in-jun <injuninjune@gmail.com>
2025-01-21 10:29:31 +0900

    feat: (#5) OpenFeign 에러 코드 추가
    OpenFeign Client에서 발생할 수 있는 에러에 대한 코드를 추가
    
    - `FEIGN_BAD_REQUEST`: 외부 API 요청이 잘못된 경우
    - `FEIGN_SERVER_ERROR`: 외부 서버 에러 발생 경우

--- src/main/kotlin/org/example/root_be/global/err/exception/ErrorCode.kt
+++ src/main/kotlin/org/example/root_be/global/err/exception/ErrorCode.kt
@@ -7,5 +7,7 @@ enum class ErrorCode(
     INVALID_TOKEN("Invalid Token", 401),
     EXPIRED_TOKEN("Expired Token", 401),
     USER_NOT_FOUND("User Not Found", 404),
-    INVALID_PASSWORD("Invalid Password", 401)
+    INVALID_PASSWORD("Invalid Password", 401),
+    FEIGN_BAD_REQUEST("External API request is invalid.", 400),
+    FEIGN_SERVER_ERROR("An external server error occurred.", 500)
 }
