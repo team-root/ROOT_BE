6e743aea5666ef218981fdd1c0d66756b9a8844a
in-jun <me@injun.dev>
2025-01-21 10:13:12 +0900

    feat: (#5) OpenFeign 의존성 추가
    OpenFeign을 사용하기 위해 spring-cloud-starter-openfeign 의존성을 추가함
    
    - 외부 API 연동을 위해 OpenFeign 사용 예정

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
