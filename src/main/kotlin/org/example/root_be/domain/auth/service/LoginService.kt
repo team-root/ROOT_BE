669121aca627bed0177f1d1d7e7b80639d25a744
in-jun <injuninjune@gmail.com>
2025-01-21 11:01:11 +0900

    feat: (#5) 로그인 기능 추가
    로그인 API 및 서비스 로직 추가
    - OpenFeign Client를 이용하여 DSM Auth 서버와 통신
    - 로그인 성공 시 Access Token, Refresh Token 발급
    - 데이터베이스에 유저 정보 저장 및 업데이트

--- src/main/kotlin/org/example/root_be/domain/auth/service/LoginService.kt
+++ src/main/kotlin/org/example/root_be/domain/auth/service/LoginService.kt
@@ -1,8 +1,69 @@
 package org.example.root_be.domain.auth.service
 
+import org.example.root_be.domain.auth.domain.RefreshToken
+import org.example.root_be.domain.auth.domain.repository.RefreshTokenRepository
+import org.example.root_be.domain.auth.presentation.dto.request.LoginRequest
+import org.example.root_be.domain.auth.presentation.dto.response.LoginResponse
+import org.example.root_be.domain.user.domain.User
+import org.example.root_be.domain.user.domain.repository.UserRepository
+import org.example.root_be.domain.user.domain.type.Role
+import org.example.root_be.global.security.jwt.JwtProperties
+import org.example.root_be.global.security.jwt.JwtTokenProvider
+import org.example.root_be.global.utils.openfeign.client.DsmAuthClient
+import org.example.root_be.global.utils.openfeign.client.dto.request.UserInfoRequest
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class LoginService {
+class LoginService(
+    private val userRepository: UserRepository,
+    private val refreshTokenRepository: RefreshTokenRepository,
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val jwtProperties: JwtProperties,
+    private val dsmAuthClient: DsmAuthClient
+) {
+    @Transactional
+    fun execute(loginRequest: LoginRequest): LoginResponse {
+        val userInfo = dsmAuthClient.getUserInfo(
+            UserInfoRequest(
+                accountId = loginRequest.xquareId,
+                password = loginRequest.password
+            )
+        )
 
+        val user = userRepository.findByDsmId(userInfo.accountId)
+            ?.apply {
+                name = userInfo.name
+                grade = userInfo.grade
+                classNum = userInfo.classNum
+            } ?: userRepository.save(
+            User(
+                id = 0L,
+                dsmId = userInfo.accountId,
+                name = userInfo.name,
+                num = userInfo.num,
+                grade = userInfo.grade,
+                classNum = userInfo.classNum,
+                userRole = Role.valueOf(userInfo.userRole),
+                totalVolunteerTime = 0
+            )
+        )
+
+        val accessToken = jwtTokenProvider.generateAccessToken(user.id)
+        val refreshToken = jwtTokenProvider.generateRefreshToken(user.id)
+
+        refreshTokenRepository.save(
+            RefreshToken(
+                id = user.id.toString(),
+                token = refreshToken,
+                ttl = jwtProperties.refreshExpiration,
+                userId = user.id
+            )
+        )
+
+        return LoginResponse(
+            accessToken = accessToken,
+            refreshToken = refreshToken
+        )
+    }
 }
