ec13c2f12510ca1dc461a88aff6f492b78938757
in-jun <me@injun.dev>
2025-01-21 10:23:51 +0900

    fix: (#5) OpenFeign OkHttpClient에서 HttpClient로 변경
    OpenFeign Client에서 OkHttpClient를 사용할 경우 발생하는 SSLHandshakeException
    오류를 해결하기 위해 HttpClient로 변경
    
    - feign-okhttp 의존성 제거
    - feign-httpclient 의존성 추가
    - SchoolUserInfoResponse에서 UserInfoResponse로 네이밍 변경

--- src/main/kotlin/org/example/root_be/global/utils/openfeign/client/DsmAuthClient.kt
+++ src/main/kotlin/org/example/root_be/global/utils/openfeign/client/DsmAuthClient.kt
@@ -1,12 +1,11 @@
 package org.example.root_be.global.utils.openfeign.client
 
 import org.example.root_be.domain.auth.presentation.dto.request.LoginRequestDto
-import org.example.root_be.global.utils.openfeign.client.dto.response.SchoolUserInfoResponse
-import org.springframework.cloud.openfeign.FeignClient
+import org.example.root_be.global.utils.openfeign.client.dto.response.UserInfoResponse
 import org.springframework.web.bind.annotation.PostMapping
 
 @FeignClient(name = "school-auth", url = "\${school.auth.url}")
 interface DsmAuthClient {
    @PostMapping("/user-auth")
-   fun getUserInfo(request: LoginRequestDto): SchoolUserInfoResponse
+   fun getUserInfo(request: LoginRequestDto): UserInfoResponse
 }
--- src/main/kotlin/org/example/root_be/global/utils/openfeign/client/dto/response/UserInfoResponse.kt
+++ src/main/kotlin/org/example/root_be/global/utils/openfeign/client/dto/response/UserInfoResponse.kt
@@ -1,6 +1,6 @@
 package org.example.root_be.global.utils.openfeign.client.dto.response
 
-data class SchoolUserInfoResponse(
+data class UserInfoResponse(
    val name: String,
    val grade: Int,
    val classNum: Int
