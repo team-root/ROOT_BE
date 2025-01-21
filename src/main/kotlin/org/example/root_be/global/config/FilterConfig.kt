cd342ecd093a2cbc3e7e4d1f7c1379c1130e8545
in-jun <injuninjune@gmail.com>
2025-01-22 08:45:31 +0900

    refactor: (#5) JwtFilter, SecurityConfig 의존성 변경
    JwtFilter, SecurityConfig에서 불필요한 의존성을 제거하고,
    FilterConfig를 통해 주입하도록 변경
    
    - JwtProperties, AuthDetailsService 의존성 제거
    - ObjectMapper 의존성 추가
    - 기존에 직접 주입받던 의존성들을 FilterConfig를 통해 주입받도록 변경
    - 이를 통해 코드의 가독성 및 유지보수성 향상

--- src/main/kotlin/org/example/root_be/global/config/FilterConfig.kt
+++ src/main/kotlin/org/example/root_be/global/config/FilterConfig.kt
@@ -2,9 +2,7 @@ package org.example.root_be.global.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.example.root_be.global.err.GlobalExceptionFilter
-import org.example.root_be.global.security.auth.AuthDetailsService
 import org.example.root_be.global.security.jwt.JwtFilter
-import org.example.root_be.global.security.jwt.JwtProperties
 import org.example.root_be.global.security.jwt.JwtTokenProvider
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -15,13 +13,11 @@ import org.springframework.stereotype.Component
 @Component
 class FilterConfig(
     private val objectMapper: ObjectMapper,
-    private val jwtProperties: JwtProperties,
     private val jwtTokenProvider: JwtTokenProvider,
-    private val authDetailsService: AuthDetailsService
 ) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
 
     override fun configure(builder: HttpSecurity) {
-        builder.addFilterBefore(JwtFilter(jwtProperties, jwtTokenProvider, authDetailsService), UsernamePasswordAuthenticationFilter::class.java)
+        builder.addFilterBefore(JwtFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter::class.java)
         builder.addFilterBefore(GlobalExceptionFilter(objectMapper), JwtFilter::class.java)
     }
 }
