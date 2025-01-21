01dbd5328e7fd10c81f2343d87b139ab00964fbe
in-jun <me@injun.dev>
2025-01-21 12:04:50 +0900

    refactor: (#5) JwtProperties 클래스 data class로 변경
    JwtProperties 클래스를 data class로 변경하여 불필요한 코드를 줄이고 가독성을 향상시킴
    
    - 기존 클래스에서 data class로 변경
    - 불필요한 getter, setter 메소드 제거

--- src/main/kotlin/org/example/root_be/global/security/jwt/JwtProperties.kt
+++ src/main/kotlin/org/example/root_be/global/security/jwt/JwtProperties.kt
@@ -2,11 +2,11 @@ package org.example.root_be.global.security.jwt
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "jwt")
-class JwtProperties {
-    var header: String = ""
-    var prefix: String = ""
-    var secret: String = ""
-    var accessExpiration: Long = 0
-    var refreshExpiration: Long = 0
-}
+@ConfigurationProperties("jwt")
+data class JwtProperties(
+    val header: String = "",
+    val prefix: String = "",
+    val secret: String = "",
+    val accessExpiration: Long = 0,
+    val refreshExpiration: Long = 0
+)
