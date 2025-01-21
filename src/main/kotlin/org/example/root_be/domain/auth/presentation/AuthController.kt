b598a3cb5cec8d50fc64f8982581ec80ab44ed5a
in-jun <injuninjune@gmail.com>
2025-01-21 10:42:13 +0900

    feat: (#5) Refresh API Request Body 추가
    Refresh API 요청 시 Request Body를 통해 refresh token을 받도록 변경
    
    - 기존 Header를 통해 refresh token을 받던 방식에서 Request Body를
      통해 받도록 변경
    - OpenFeign Client URL 및 DTO 수정
    - 이를 통해 보안 및 API 명세의 일관성 향상

--- src/main/kotlin/org/example/root_be/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/org/example/root_be/domain/auth/presentation/AuthController.kt
@@ -1,5 +1,6 @@
 package org.example.root_be.domain.auth.presentation
 
+import org.example.root_be.domain.auth.presentation.dto.request.RefreshRequest
 import org.example.root_be.domain.auth.presentation.dto.response.RefreshResponse
 import org.example.root_be.domain.auth.service.RefreshService
 import org.springframework.web.bind.annotation.*
@@ -10,6 +11,6 @@ class AuthController(
     private val refreshService: RefreshService
 ) {
     @PostMapping("/refresh")
-    fun refresh(@RequestHeader("X-Refresh-Token") refreshToken: String): RefreshResponse =
-        refreshService.execute(refreshToken)
+    fun refresh(@RequestBody refreshRequest: RefreshRequest): RefreshResponse =
+        refreshService.execute(refreshRequest)
 }
--- src/main/kotlin/org/example/root_be/global/utils/openfeign/client/dto/response/UserInfoResponse.kt
+++ src/main/kotlin/org/example/root_be/global/utils/openfeign/client/dto/response/UserInfoResponse.kt
@@ -1,7 +1,12 @@
 package org.example.root_be.global.utils.openfeign.client.dto.response
 
 data class UserInfoResponse(
+   val id: String,
+   val accountId: String,
+   val password: String,
    val name: String,
    val grade: Int,
-   val classNum: Int
+   val classNum: Int,
+   val num: Int,
+   val userRole: String
 )
