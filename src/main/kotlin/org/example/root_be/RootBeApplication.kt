9ee3b6b981d933f185222f21aad16941a924ec6a
in-jun <me@injun.dev>
2025-01-21 11:20:53 +0900

    feat: (#5) OpenFeign 설정 추가
    DSM Auth API 연동을 위한 OpenFeign Client 설정을 추가하고,
    application.yml 에 DSM Auth URL 설정을 추가함
    
    - `@EnableFeignClients` 어노테이션 추가
    - dsm.auth.url 설정 추가

--- src/main/kotlin/org/example/root_be/RootBeApplication.kt
+++ src/main/kotlin/org/example/root_be/RootBeApplication.kt
@@ -2,7 +2,9 @@ package org.example.root_be
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
+@EnableFeignClients
 @SpringBootApplication
 class RootBeApplication
 
