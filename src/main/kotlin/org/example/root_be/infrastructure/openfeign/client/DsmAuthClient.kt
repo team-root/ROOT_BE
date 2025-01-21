cc1de943e12ca9ad6b56b2e461e02da78e5e0be1
in-jun <me@injun.dev>
2025-01-22 08:15:24 +0900

    refactor: (#5) OpenFeign 관련 패키지 구조 변경
    OpenFeign 관련 클래스들을 `global/utils`에서 `infrastructure`
    패키지로 이동
    
    - OpenFeign 기능이 외부 시스템과의 연동을 담당하는
      인프라 계층에 속한다고 판단하여 패키지 이동
    - 향후 인프라 관련 코드들을 `infrastructure` 패키지
      하위에 추가 관리 예정

--- src/main/kotlin/org/example/root_be/infrastructure/openfeign/client/DsmAuthClient.kt
+++ src/main/kotlin/org/example/root_be/infrastructure/openfeign/client/DsmAuthClient.kt
@@ -1,8 +1,8 @@
-package org.example.root_be.global.utils.openfeign.client
+package org.example.root_be.infrastructure.openfeign.client
 
-import org.example.root_be.global.utils.openfeign.client.dto.request.UserInfoRequest
+import org.example.root_be.infrastructure.openfeign.client.dto.request.UserInfoRequest
 import org.springframework.cloud.openfeign.FeignClient;
-import org.example.root_be.global.utils.openfeign.client.dto.response.UserInfoResponse
+import org.example.root_be.infrastructure.openfeign.client.dto.response.UserInfoResponse
 import org.springframework.web.bind.annotation.PostMapping
 
 @FeignClient(name = "dsm-auth", url = "\${dsm.auth.url}")
--- src/main/kotlin/org/example/root_be/infrastructure/openfeign/client/dto/request/UserInfoRequest.kt
+++ src/main/kotlin/org/example/root_be/infrastructure/openfeign/client/dto/request/UserInfoRequest.kt
@@ -1,4 +1,4 @@
-package org.example.root_be.global.utils.openfeign.client.dto.request
+package org.example.root_be.infrastructure.openfeign.client.dto.request
 
 data class UserInfoRequest(
     val accountId: String,
