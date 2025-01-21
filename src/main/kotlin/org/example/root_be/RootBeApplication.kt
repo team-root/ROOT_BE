aa32c396c63fa3ecb0d01af1611ca534502b7292
in-jun <me@injun.dev>
2025-01-21 11:51:42 +0900

    feat: (#5) JWT 설정 파일 추가
    JwtProperties 클래스를 추가하고, `@ConfigurationPropertiesScan` 어노테이션을 통해 스프링 빈으로 등록하여 application.yml 파일에서 JWT 관련 설정값을 가져올 수 있도록 함
    
    - 기존에는 JwtProperties 클래스에 `@Component` 어노테이션을 사용하여 스프링 빈으로 등록했었음
    - `@ConfigurationProperties` 어노테이션을 사용하기 위해 `@Component` 어노테이션 제거 및 data class 에서 일반 클래스로 변경
    - RootBeApplication 클래스에 `@ConfigurationPropertiesScan` 어노테이션 추가
    - 이를 통해 외부 설정 파일에서 JWT 관련 설정값을 읽어와 사용할 수 있도록 개선

--- src/main/kotlin/org/example/root_be/RootBeApplication.kt
+++ src/main/kotlin/org/example/root_be/RootBeApplication.kt
@@ -1,11 +1,13 @@
 package org.example.root_be
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 
-@EnableFeignClients
 @SpringBootApplication
+@EnableFeignClients
+@ConfigurationPropertiesScan
 class RootBeApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/org/example/root_be/global/security/jwt/JwtProperties.kt
+++ src/main/kotlin/org/example/root_be/global/security/jwt/JwtProperties.kt
@@ -1,14 +1,12 @@
 package org.example.root_be.global.security.jwt
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.stereotype.Component
 
-@Component
 @ConfigurationProperties(prefix = "jwt")
-data class JwtProperties(
-    val header: String,
-    val prefix: String,
-    val secret: String,
-    val accessExpiration: Long,
-    val refreshExpiration: Long
-)
+class JwtProperties {
+    var header: String = ""
+    var prefix: String = ""
+    var secret: String = ""
+    var accessExpiration: Long = 0
+    var refreshExpiration: Long = 0
+}
