99773b32503813ae003ee55fe907e151cee365e7
in-jun <me@injun.dev>
2025-01-23 07:24:49 +0900

    feat: (#10) 마이페이지 조회 기능 추가
    마이페이지에서 유저 정보를 조회하는 기능을 추가한다.
    
    - 학번, 이름, 봉사 시간 정보를 조회할 수 있도록 구현
    - 추후 추가적인 정보 표시 가능

---
+++ src/main/kotlin/org/example/root_be/domain/user/presentation/dto/response/MypageResponse.kt
@@ -0,0 +1,7 @@
+package org.example.root_be.domain.user.presentation.dto.response
+
+data class MypageResponse (
+    val studentNumber: Int,
+    val studentName: String,
+    val volunteerTime: Int
+)
