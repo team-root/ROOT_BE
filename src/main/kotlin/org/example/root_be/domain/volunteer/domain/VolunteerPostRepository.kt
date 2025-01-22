598d9a969dd04ea8358805da2942db5f44de926e
도훈 <modooboiroo@gmail.com>
2025-01-22 11:27:21 +0900

    feat: (#7) VolunteerPostRepository

---
+++ src/main/kotlin/org/example/root_be/domain/volunteer/domain/VolunteerPostRepository.kt
@@ -0,0 +1,5 @@
+package org.example.root_be.domain.volunteer.domain
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface VolunteerPostRepository: JpaRepository<VolunteerPost, Long>
