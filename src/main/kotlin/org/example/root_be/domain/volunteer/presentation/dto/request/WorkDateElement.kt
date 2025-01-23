3d2cbf2a15aebc6bcd8d9fa6bfcc319dfef76f74
도훈 <modooboiroo@gmail.com>
2025-01-24 02:13:01 +0900

    feat: (#7) @JsonFormat 추가

--- src/main/kotlin/org/example/root_be/domain/volunteer/presentation/dto/request/WorkDateElement.kt
+++ src/main/kotlin/org/example/root_be/domain/volunteer/presentation/dto/request/WorkDateElement.kt
@@ -1,8 +1,11 @@
 package org.example.root_be.domain.volunteer.presentation.dto.request
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 
 data class WorkDateElement(
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     val startDate: LocalDateTime,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     val endDate: LocalDateTime
 )
