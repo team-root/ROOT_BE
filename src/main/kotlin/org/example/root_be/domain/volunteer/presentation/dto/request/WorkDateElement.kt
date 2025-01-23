8a9b011c0951795eb1d67e027b1f8e66b0f2439b
도훈 <modooboiroo@gmail.com>
2025-01-24 02:51:37 +0900

    feat: (#7) LocalDate로 타입 수정

--- src/main/kotlin/org/example/root_be/domain/volunteer/presentation/dto/request/WorkDateElement.kt
+++ src/main/kotlin/org/example/root_be/domain/volunteer/presentation/dto/request/WorkDateElement.kt
@@ -1,11 +1,11 @@
 package org.example.root_be.domain.volunteer.presentation.dto.request
 
 import com.fasterxml.jackson.annotation.JsonFormat
-import java.time.LocalDateTime
+import java.time.LocalDate
 
 data class WorkDateElement(
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    val startDate: LocalDateTime,
+    val startDate: LocalDate,
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    val endDate: LocalDateTime
+    val endDate: LocalDate
 )
