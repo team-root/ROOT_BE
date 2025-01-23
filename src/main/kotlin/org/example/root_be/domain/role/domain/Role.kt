9d0f54e75e0ace23b625715f47edad9089373b36
도훈 <modooboiroo@gmail.com>
2025-01-24 00:35:53 +0900

    feat: (#7) Role

---
+++ src/main/kotlin/org/example/root_be/domain/role/domain/Role.kt
@@ -0,0 +1,19 @@
+package org.example.root_be.domain.role.domain
+
+import jakarta.persistence.*
+import org.example.root_be.domain.volunteer.domain.VolunteerPost
+
+@Entity
+@Table(name = "ROLE")
+class Role(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    val volunteerPost: VolunteerPost,
+
+    @Column(name = "title")
+    val title: String,
+)
