e36646af9e71d41c2c065dcaa9a95063aa640818
도훈 <modooboiroo@gmail.com>
2025-01-24 02:52:11 +0900

    feat: (#7) @JsonFormat pattern 속성 삭제

--- src/main/kotlin/org/example/root_be/domain/volunteer/presentation/dto/request/WorkDateElement.kt
+++ src/main/kotlin/org/example/root_be/domain/volunteer/presentation/dto/request/WorkDateElement.kt
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDate
 
 data class WorkDateElement(
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     val startDate: LocalDate,
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     val endDate: LocalDate
 )
