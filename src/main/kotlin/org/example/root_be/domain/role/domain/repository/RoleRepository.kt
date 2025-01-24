c1666921528f4c402d43cfa9eb903bef51141443
도훈 <149929673+coehgns@users.noreply.github.com>
2025-01-24 15:24:49 +0900

    Revert "merge: (#7) 봉사 게시물 생성 기능 추가"

--- src/main/kotlin/org/example/root_be/domain/role/domain/repository/RoleRepository.kt
+++
@@ -1,6 +0,0 @@
-package org.example.root_be.domain.role.domain.repository
-
-import org.example.root_be.domain.role.domain.Role
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface RoleRepository: JpaRepository<Role, Long>
--- src/main/kotlin/org/example/root_be/domain/volunteer/domain/repository/VolunteerPostRepository.kt
+++
@@ -1,6 +0,0 @@
-package org.example.root_be.domain.volunteer.domain.repository
-
-import org.example.root_be.domain.volunteer.domain.VolunteerPost
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface VolunteerPostRepository: JpaRepository<VolunteerPost, Long>
