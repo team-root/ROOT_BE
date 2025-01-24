9a14752304ff89ca28473a4c1aa5ac5b2a00bd62
도훈 <modooboiroo@gmail.com>
2025-01-25 02:36:07 +0900

    feat: (#22) getVolunteerPosts method

--- src/main/kotlin/org/example/root_be/domain/volunteer/presentation/VolunteerController.kt
+++ src/main/kotlin/org/example/root_be/domain/volunteer/presentation/VolunteerController.kt
@@ -1,7 +1,10 @@
 package org.example.root_be.domain.volunteer.presentation
 
 import org.example.root_be.domain.volunteer.presentation.dto.request.GenerateVolunteerPostRequest
+import org.example.root_be.domain.volunteer.presentation.dto.response.GetVolunteerPostsResponse
 import org.example.root_be.domain.volunteer.service.GenerateVolunteerPostService
+import org.example.root_be.domain.volunteer.service.GetVolunteerPostsService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,12 +13,18 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/posts")
 class VolunteerController(
-    private val generateVolunteerPostService: GenerateVolunteerPostService
+    private val generateVolunteerPostService: GenerateVolunteerPostService,
+    private val getVolunteerPostsService: GetVolunteerPostsService
 ) {
     @PostMapping
     fun generateVolunteerPost(
         @RequestBody request: GenerateVolunteerPostRequest
     ) {
         generateVolunteerPostService.execute(request)
+    }
+
+    @GetMapping
+    fun getVolunteerPosts(): GetVolunteerPostsResponse {
+        return getVolunteerPostsService.execute()
     }
 }
