3eb30b19c092c671eac457c25a7b34ddf792a21a
도훈 <modooboiroo@gmail.com>
2025-01-25 02:35:06 +0900

    feat: (#22) findBy() 메서드 추가

--- src/main/kotlin/org/example/root_be/domain/volunteer/domain/repository/VolunteerPostRepository.kt
+++ src/main/kotlin/org/example/root_be/domain/volunteer/domain/repository/VolunteerPostRepository.kt
@@ -3,4 +3,6 @@ package org.example.root_be.domain.volunteer.domain.repository
 import org.example.root_be.domain.volunteer.domain.VolunteerPost
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface VolunteerPostRepository: JpaRepository<VolunteerPost, Long>
+interface VolunteerPostRepository: JpaRepository<VolunteerPost, Long> {
+    fun findBy(): List<VolunteerPost>
+}
