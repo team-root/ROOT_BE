8e6472da6042f6885ee944b061ce7da74e97ef0b
in-jun <injuninjune@gmail.com>
2025-01-26 20:51:12 +0900

    feat: (#38) 봉사 신청 수락/거절 기능 추가
    봉사 신청을 관리자가 수락/거절 할 수 있는 기능을 추가합니다.
    
    - 봉사 신청 수락/거절 API 추가
    - ApplicationNotFoundException 추가
    - VolunteerApplicationFacade 추가
    - SecurityConfig 수정
    - ErrorCode APPLICATION_NOT_FOUND 추가

---
+++ src/main/kotlin/org/example/root_be/domain/applications/facade/VolunteerApplicationFacade.kt
@@ -0,0 +1,16 @@
+package org.example.root_be.domain.applications.facade
+
+import org.example.root_be.domain.applications.domain.VolunteerApplication
+import org.example.root_be.domain.applications.domain.repository.VolunteerApplicationRepository
+import org.example.root_be.domain.applications.exception.ApplicationNotFoundException
+import org.springframework.stereotype.Component
+
+@Component
+class VolunteerApplicationFacade(
+    private val volunteerApplicationRepository: VolunteerApplicationRepository
+) {
+    fun getVolunteerApplicationById(applicationId: Long): VolunteerApplication {
+        return volunteerApplicationRepository.findById(applicationId)
+            .orElseThrow() { ApplicationNotFoundException }
+    }
+}
