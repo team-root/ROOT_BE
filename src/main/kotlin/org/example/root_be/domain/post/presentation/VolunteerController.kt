0d2606da4570cff6ad09b65470311b84cad51ee9
도훈 <modooboiroo@gmail.com>
2025-01-26 23:31:19 +0900

    feat: (#30) VolunteerController에 modifyVolunteerPost 메서드 추가

--- src/main/kotlin/org/example/root_be/domain/post/presentation/VolunteerController.kt
+++ src/main/kotlin/org/example/root_be/domain/post/presentation/VolunteerController.kt
@@ -1,12 +1,15 @@
 package org.example.root_be.domain.post.presentation
 
 import org.example.root_be.domain.post.presentation.dto.request.GenerateVolunteerPostRequest
+import org.example.root_be.domain.post.presentation.dto.request.ModifyVolunteerPostRequest
 import org.example.root_be.domain.post.presentation.dto.response.GetVolunteerPostResponse
 import org.example.root_be.domain.post.presentation.dto.response.GetVolunteerPostsResponse
 import org.example.root_be.domain.post.service.GenerateVolunteerPostService
 import org.example.root_be.domain.post.service.GetVolunteerPostDetailsService
 import org.example.root_be.domain.post.service.GetVolunteerPostListService
+import org.example.root_be.domain.post.service.ModifyVolunteerPostService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -18,7 +21,8 @@ import org.springframework.web.bind.annotation.RestController
 class VolunteerController(
     private val generateVolunteerPostService: GenerateVolunteerPostService,
     private val getVolunteerPostsService: GetVolunteerPostListService,
-    private val getVolunteerPostService: GetVolunteerPostDetailsService
+    private val getVolunteerPostService: GetVolunteerPostDetailsService,
+    private val modifyVolunteerPostService: ModifyVolunteerPostService
 ) {
     @PostMapping
     fun generateVolunteerPost(
@@ -35,5 +39,13 @@ class VolunteerController(
     @GetMapping("/{postId}")
     fun getVolunteerPost(@PathVariable postId: Long): GetVolunteerPostResponse {
         return getVolunteerPostService.execute(postId)
+    }
+
+    @PatchMapping("/{postId}")
+    fun modifyVolunteerPost(
+        @PathVariable postId: Long,
+        @RequestBody request: ModifyVolunteerPostRequest
+    ) {
+        modifyVolunteerPostService.execute(postId, request)
     }
 }
