bab5bf0f44f5cf99c47acef1b31e2efbcdcf1a41
in-jun <me@injun.dev>
2025-01-26 19:05:10 +0900

    refactor: (#29) 봉사 신청 서비스 로직 개선
    Facade 패턴을 적용하여 봉사 신청 서비스 로직을 개선한다.
    - 기존에 Repository를 직접 사용하던 부분을 Facade를 통해 처리하도록 변경
    - 이를 통해 서비스 로직의 의존성을 줄이고, 코드의 가독성 및 유지보수성 향상
    - `VolunteerPostRepository` 의존성 제거
    - `VolunteerFacade` 의존성 추가

--- src/main/kotlin/org/example/root_be/domain/applications/service/ApplyVolunteerService.kt
+++ src/main/kotlin/org/example/root_be/domain/applications/service/ApplyVolunteerService.kt
@@ -5,28 +5,26 @@ import org.example.root_be.domain.applications.domain.VolunteerApplication
 import org.example.root_be.domain.applications.domain.repository.VolunteerApplicationRepository
 import org.example.root_be.domain.applications.presentation.dto.response.ApplyVolunteerResponse
 import org.example.root_be.domain.user.facade.UserFacade
-import org.example.root_be.domain.volunteer.domain.repository.VolunteerPostRepository
-import org.example.root_be.domain.volunteer.exception.VolunteerPostNotFoundException
+import org.example.root_be.domain.volunteer.facade.VolunteerFacade
 import org.springframework.stereotype.Service
 
 @Service
 class ApplyVolunteerService(
     private val volunteerApplicationRepository: VolunteerApplicationRepository,
     private val userFacade: UserFacade,
-    private val volunteerPostRepository: VolunteerPostRepository
+    private val volunteerFacade: VolunteerFacade
 ) {
     @Transactional
     fun execute(postId: Long): ApplyVolunteerResponse {
         val user = userFacade.getCurrentUser()
-        val post = volunteerPostRepository.findById(postId)
-            .orElseThrow() { VolunteerPostNotFoundException }
+        val post = volunteerFacade.getVolunteerPostById(postId)
 
-        val application = VolunteerApplication(
-            user = user,
-            volunteerPost = post
+        val savedApplication = volunteerApplicationRepository.save(
+            VolunteerApplication(
+                user = user,
+                volunteerPost = post
+            )
         )
-
-        val savedApplication = volunteerApplicationRepository.save(application)
 
         return ApplyVolunteerResponse(
             applicationId = savedApplication.id,
