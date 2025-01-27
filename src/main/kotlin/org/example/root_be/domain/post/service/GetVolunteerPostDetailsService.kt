794c0421a3ffae09a638f4f245d8b0438a513a17
도훈 <modooboiroo@gmail.com>
2025-01-28 00:26:35 +0900

    refactor: (#45) GetVolunteerPostDetailsService

--- src/main/kotlin/org/example/root_be/domain/post/service/GetVolunteerPostDetailsService.kt
+++ src/main/kotlin/org/example/root_be/domain/post/service/GetVolunteerPostDetailsService.kt
@@ -1,19 +1,22 @@
 package org.example.root_be.domain.post.service
 
 import jakarta.transaction.Transactional
+import org.example.root_be.domain.detail.facade.DetailFacade
 import org.example.root_be.domain.post.facade.VolunteerFacade
 import org.example.root_be.domain.post.presentation.dto.response.GetVolunteerPostResponse
 import org.springframework.stereotype.Service
 
 @Service
 class GetVolunteerPostDetailsService(
-    private val volunteerFacade: VolunteerFacade
+    private val volunteerFacade: VolunteerFacade,
+    private val detailsFacade: DetailFacade
 ) {
     @Transactional
     fun execute(
         postId: Long
     ): GetVolunteerPostResponse {
         val volunteerPost = volunteerFacade.getVolunteerPostById(postId)
-        return GetVolunteerPostResponse(volunteerPost)
+        val volunteerDetail = detailsFacade.getVolunteerDetailsByPostId(postId)
+        return GetVolunteerPostResponse(volunteerPost, volunteerDetail)
     }
 }
