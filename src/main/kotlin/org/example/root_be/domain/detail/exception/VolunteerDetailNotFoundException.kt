94860dadba84c40d48cb69d31f5813c4ef900d79
도훈 <modooboiroo@gmail.com>
2025-01-28 00:26:10 +0900

    feat: (#45) VolunteerDetailNotFoundException

---
+++ src/main/kotlin/org/example/root_be/domain/detail/exception/VolunteerDetailNotFoundException.kt
@@ -0,0 +1,6 @@
+package org.example.root_be.domain.detail.exception
+
+import org.example.root_be.global.err.exception.CustomException
+import org.example.root_be.global.err.exception.ErrorCode
+
+object VolunteerDetailNotFoundException: CustomException(ErrorCode.VOLUNTEER_DETAIL_NOT_FOUND)
