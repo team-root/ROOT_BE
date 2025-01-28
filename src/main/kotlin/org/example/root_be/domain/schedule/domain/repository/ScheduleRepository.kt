c5764a5e1ba899d194fcc07e7103bc3d03439139
도훈 <modooboiroo@gmail.com>
2025-01-28 21:33:16 +0900

    feat: (#52) ScheduleRepository에 findBy() 메서드 추가

--- src/main/kotlin/org/example/root_be/domain/schedule/domain/repository/ScheduleRepository.kt
+++ src/main/kotlin/org/example/root_be/domain/schedule/domain/repository/ScheduleRepository.kt
@@ -3,4 +3,6 @@ package org.example.root_be.domain.schedule.domain.repository
 import org.example.root_be.domain.schedule.domain.Schedule
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ScheduleRepository: JpaRepository<Schedule, Long>
+interface ScheduleRepository: JpaRepository<Schedule, Long> {
+    fun findBy(): List<Schedule>
+}
