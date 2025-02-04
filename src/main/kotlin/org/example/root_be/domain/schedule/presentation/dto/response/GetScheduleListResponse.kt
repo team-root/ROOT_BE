fb0e3da9f40121ce588a5e81cc5f61a09fd1ea28
도훈 <modooboiroo@gmail.com>
2025-02-04 10:08:26 +0900

    refactor: (#55) ScheduleResponse에 id 필드 추가

--- src/main/kotlin/org/example/root_be/domain/schedule/presentation/dto/response/GetScheduleListResponse.kt
+++ src/main/kotlin/org/example/root_be/domain/schedule/presentation/dto/response/GetScheduleListResponse.kt
@@ -1,12 +1,12 @@
 package org.example.root_be.domain.schedule.presentation.dto.response
 
-import org.example.root_be.domain.schedule.domain.Schedule
 import java.time.LocalDate
 
 data class GetScheduleListResponse(
     val events: List<ScheduleResponse>
 ) {
     data class ScheduleResponse(
+        val id: Long,
         val title: String,
         val startDate: LocalDate,
         val endDate: LocalDate,
