3eb58cec5c226485b0af3682e396a85eae5450bd
도훈 <modooboiroo@gmail.com>
2025-02-04 10:39:01 +0900

    feat: (#57) GetScheduleResponse

---
+++ src/main/kotlin/org/example/root_be/domain/schedule/presentation/dto/response/GetScheduleResponse.kt
@@ -0,0 +1,9 @@
+package org.example.root_be.domain.schedule.presentation.dto.response
+
+import java.time.LocalDate
+
+data class GetScheduleResponse(
+    val id: Long,
+    val startDate: LocalDate,
+    val endDate: LocalDate
+)
