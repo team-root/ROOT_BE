8d7b0a55156fb7548d013c5bf168f9a11f967c00
도훈 <modooboiroo@gmail.com>
2025-02-07 04:33:26 +0900

    feat: (#57) ErrorCode에 SCHEDULE_NOT_FOUND 추가

--- src/main/kotlin/org/example/root_be/global/err/exception/ErrorCode.kt
+++ src/main/kotlin/org/example/root_be/global/err/exception/ErrorCode.kt
@@ -10,6 +10,7 @@ enum class ErrorCode(
     VOLUNTEER_POST_NOT_FOUND("Post Not Found", 404),
     VOLUNTEER_ROLE_NOT_FOUND("Role Not Found", 404),
     VOLUNTEER_DETAIL_NOT_FOUND("Detail Not Found", 404),
+    SCHEDULE_NOT_FOUND("Schedule Not Found", 404),
     USER_NOT_FOUND("User Not Found", 404),
     INVALID_PASSWORD("Invalid Password", 401),
     FEIGN_BAD_REQUEST("External API request is invalid.", 400),
