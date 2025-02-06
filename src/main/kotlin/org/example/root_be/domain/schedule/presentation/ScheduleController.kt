6466c57dcb392f2fcf19cb411a6c7f7689e14c75
도훈 <modooboiroo@gmail.com>
2025-02-07 02:52:48 +0900

    feat: (#57) ScheduleController에 getSchedule 메서드 추가

--- src/main/kotlin/org/example/root_be/domain/schedule/presentation/ScheduleController.kt
+++ src/main/kotlin/org/example/root_be/domain/schedule/presentation/ScheduleController.kt
@@ -2,19 +2,24 @@ package org.example.root_be.domain.schedule.presentation
 
 import org.example.root_be.domain.schedule.presentation.dto.request.GenerateScheduleRequest
 import org.example.root_be.domain.schedule.presentation.dto.response.GetScheduleListResponse
+import org.example.root_be.domain.schedule.presentation.dto.response.GetScheduleResponse
 import org.example.root_be.domain.schedule.service.GenerateScheduleService
 import org.example.root_be.domain.schedule.service.GetScheduleListService
+import org.example.root_be.domain.schedule.service.GetScheduleService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
 @RestController
 @RequestMapping("/schedules")
 class ScheduleController(
     private val generateScheduleService: GenerateScheduleService,
-    private val getScheduleListService: GetScheduleListService
+    private val getScheduleListService: GetScheduleListService,
+    private val getScheduleService: GetScheduleService
 ) {
     @PostMapping
     fun generateSchedule(
@@ -26,5 +31,12 @@ class ScheduleController(
     @GetMapping
     fun getScheduleList(): GetScheduleListResponse {
         return getScheduleListService.execute()
+    }
+
+    @GetMapping("/{date}")
+    fun getSchedule(
+        @PathVariable date: LocalDate
+    ): GetScheduleResponse {
+        return getScheduleService.execute(date)
     }
 }
