976ce4efa3548a3ed58712cbe1492ce5200610dc
도훈 <modooboiroo@gmail.com>
2025-02-07 04:33:46 +0900

    feat: (#57) ScheduleFacade

---
+++ src/main/kotlin/org/example/root_be/domain/schedule/facade/ScheduleFacade.kt
@@ -0,0 +1,19 @@
+package org.example.root_be.domain.schedule.facade
+
+import org.example.root_be.domain.schedule.domain.Schedule
+import org.example.root_be.domain.schedule.domain.repository.ScheduleRepository
+import org.example.root_be.domain.schedule.exception.ScheduleNotFoundException
+import org.springframework.stereotype.Component
+import java.time.LocalDate
+
+@Component
+class ScheduleFacade(
+    private val scheduleRepository: ScheduleRepository
+) {
+    fun findScheduleByDate(
+        date: LocalDate
+    ): Schedule {
+        return scheduleRepository.findByDate(date)
+            ?: throw ScheduleNotFoundException
+    }
+}
