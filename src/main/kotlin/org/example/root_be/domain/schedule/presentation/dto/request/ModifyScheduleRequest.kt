ba53ad0918277c1ef9c9585bbba8b30c4276ab34
도훈 <modooboiroo@gmail.com>
2025-02-07 22:02:24 +0900

    feat: (#65) ModifyScheduleRequest

---
+++ src/main/kotlin/org/example/root_be/domain/schedule/presentation/dto/request/ModifyScheduleRequest.kt
@@ -0,0 +1,9 @@
+package org.example.root_be.domain.schedule.presentation.dto.request
+
+import java.time.LocalDate
+
+data class ModifyScheduleRequest(
+    val title: String,
+    val startDate: LocalDate,
+    val endDate: LocalDate
+)
