6c7e78f97f56464b37db2cbb5c881918b50cc06f
도훈 <modooboiroo@gmail.com>
2025-02-08 23:12:10 +0900

    feat: (#65) ScheduleController에 modifySchedule 메서드 추가

--- src/main/kotlin/org/example/root_be/domain/schedule/presentation/ScheduleController.kt
+++ src/main/kotlin/org/example/root_be/domain/schedule/presentation/ScheduleController.kt
@@ -1,12 +1,15 @@
 package org.example.root_be.domain.schedule.presentation
 
 import org.example.root_be.domain.schedule.presentation.dto.request.GenerateScheduleRequest
+import org.example.root_be.domain.schedule.presentation.dto.request.ModifyScheduleRequest
 import org.example.root_be.domain.schedule.presentation.dto.response.GetScheduleListResponse
 import org.example.root_be.domain.schedule.presentation.dto.response.GetScheduleResponse
 import org.example.root_be.domain.schedule.service.GenerateScheduleService
 import org.example.root_be.domain.schedule.service.GetScheduleListService
 import org.example.root_be.domain.schedule.service.GetScheduleService
+import org.example.root_be.domain.schedule.service.ModifyScheduleService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -19,7 +22,8 @@ import java.time.LocalDate
 class ScheduleController(
     private val generateScheduleService: GenerateScheduleService,
     private val getScheduleListService: GetScheduleListService,
-    private val getScheduleService: GetScheduleService
+    private val getScheduleService: GetScheduleService,
+    private val modifyScheduleService: ModifyScheduleService
 ) {
     @PostMapping
     fun generateSchedule(
@@ -38,5 +42,13 @@ class ScheduleController(
         @PathVariable date: LocalDate
     ): GetScheduleResponse {
         return getScheduleService.execute(date)
+    }
+
+    @PatchMapping("/{date}")
+    fun modifySchedule(
+        @PathVariable date: LocalDate,
+        @RequestBody request: ModifyScheduleRequest
+    ) {
+        modifyScheduleService.execute(date, request)
     }
 }
