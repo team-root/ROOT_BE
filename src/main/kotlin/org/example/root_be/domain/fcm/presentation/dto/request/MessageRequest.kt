01c8ba4c9cf5680e65362396d0ead6fb10cd5612
도훈 <modooboiroo@gmail.com>
2025-02-11 11:38:12 +0900

    feat: (#71) MessageRequest

---
+++ src/main/kotlin/org/example/root_be/domain/fcm/presentation/dto/request/MessageRequest.kt
@@ -0,0 +1,14 @@
+package org.example.root_be.domain.fcm.presentation.dto.request
+
+import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.Length
+
+data class MessageRequest(
+    @field:NotBlank(message = "제목을 비워둘 수 없습니다.")
+    @field:Length(min = 1, max = 30, message = "제목은 최소 1글자, 최대 30글자까지 작성 가능합니다.")
+    val title: String,
+
+    @field:NotBlank(message = "설명을 비워둘 수 없습니다.")
+    @field:Length(min = 1, max = 20, message = "설명은 최소 1글자, 최대 20글자까지 작성 가능합니다.")
+    val body: String
+)
