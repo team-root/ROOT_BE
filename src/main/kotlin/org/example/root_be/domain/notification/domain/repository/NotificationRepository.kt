ac85eee53a819b7fa83b84869f1683c02694add0
도훈 <modooboiroo@gmail.com>
2025-02-11 21:04:29 +0900

    feat: (#73) NotificationRepository에 findBy 메서드 추가

--- src/main/kotlin/org/example/root_be/domain/notification/domain/repository/NotificationRepository.kt
+++ src/main/kotlin/org/example/root_be/domain/notification/domain/repository/NotificationRepository.kt
@@ -3,4 +3,6 @@ package org.example.root_be.domain.notification.domain.repository
 import org.example.root_be.domain.notification.domain.Notification
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface NotificationRepository: JpaRepository<Notification, Long>
+interface NotificationRepository: JpaRepository<Notification, Long> {
+    fun findBy(): List<Notification>
+}
