ba97d5c2f6994b9ea3d2d8db3c8f0bf4eb528d8b
도훈 <modooboiroo@gmail.com>
2025-02-11 11:33:50 +0900

    feat: (#71) LoginRequest에 fcmToken 필드 추가

--- src/main/kotlin/org/example/root_be/domain/auth/presentation/dto/request/LoginRequest.kt
+++ src/main/kotlin/org/example/root_be/domain/auth/presentation/dto/request/LoginRequest.kt
@@ -3,6 +3,8 @@ package org.example.root_be.domain.auth.presentation.dto.request
 import jakarta.validation.constraints.NotBlank
 
 data class LoginRequest(
+    val fcmToken: String?,
+
     @NotBlank(message = "xquareId는 필수 값입니다")
     val xquareId: String,
 
