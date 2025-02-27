48e24f5253147d1536e49e6d3383cc347f70705f
coehgns <modoobiroo@gmail.com>
2025-02-27 23:09:05 +0900

    refactor: (#81) controller에 @ResponseStatus 추가

--- src/main/kotlin/org/example/root_be/domain/schedule/presentation/ScheduleController.kt
+++ src/main/kotlin/org/example/root_be/domain/schedule/presentation/ScheduleController.kt
@@ -6,15 +6,9 @@ import org.example.root_be.domain.schedule.presentation.dto.request.ModifySchedu
 import org.example.root_be.domain.schedule.presentation.dto.response.GetScheduleListResponse
 import org.example.root_be.domain.schedule.presentation.dto.response.GetScheduleResponse
 import org.example.root_be.domain.schedule.service.*
+import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.time.LocalDate
 
 @RestController
@@ -28,6 +22,7 @@ class ScheduleController(
     private val deleteScheduleService: DeleteScheduleService
 ) {
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun generateSchedule(
         @Valid
         @RequestBody request: GenerateScheduleRequest
