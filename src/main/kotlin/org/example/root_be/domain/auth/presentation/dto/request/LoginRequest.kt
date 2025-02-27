906ca87ea24e650782a08ec6322764db7004698a
coehgns <modoobiroo@gmail.com>
2025-02-27 22:39:31 +0900

    refactor: (#81) request에 @field 추가

--- src/main/kotlin/org/example/root_be/domain/auth/presentation/dto/request/LoginRequest.kt
+++ src/main/kotlin/org/example/root_be/domain/auth/presentation/dto/request/LoginRequest.kt
@@ -5,9 +5,9 @@ import jakarta.validation.constraints.NotBlank
 data class LoginRequest(
     val deviceToken: String?,
 
-    @NotBlank(message = "xquareId는 필수 값입니다")
+    @field:NotBlank(message = "xquareId는 필수 값입니다")
     val xquareId: String,
 
-    @NotBlank(message = "비밀번호는 필수 값입니다")
+    @field:NotBlank(message = "비밀번호는 필수 값입니다")
     val password: String
 )
--- src/main/kotlin/org/example/root_be/domain/user/presentation/dto/request/AddStudentVolunteerRecordRequest.kt
+++ src/main/kotlin/org/example/root_be/domain/user/presentation/dto/request/AddStudentVolunteerRecordRequest.kt
@@ -6,16 +6,16 @@ import jakarta.validation.constraints.NotEmpty
 import org.jetbrains.annotations.NotNull
 
 data class AddStudentVolunteerRecordRequest(
-    @NotEmpty(message = "사용자 ID 목록은 필수입니다")
+    @field:NotEmpty(message = "사용자 ID 목록은 필수입니다")
     val userIds: List<Long>,
 
-    @NotBlank(message = "활동 상세내용은 필수입니다")
+    @field:NotBlank(message = "활동 상세내용은 필수입니다")
     val detail: String,
 
-    @NotNull
+    @field:NotNull
     @Min(value = 1, message = "봉사시간은 1시간 이상이어야 합니다")
     val time: Int,
 
-    @NotBlank(message = "봉사장소는 필수입니다")
+    @field:NotBlank(message = "봉사장소는 필수입니다")
     val place: String
 )
