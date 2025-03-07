ddc3812d9e5ceb1fd6f0f621ec06f17c4948eeda
coehgns <modoobiroo@gmail.com>
2025-03-07 14:27:36 +0900

    feat: (#88) findAllByVolunteerPost method 추가

--- src/main/kotlin/org/example/rootbe/domain/applications/domain/repository/VolunteerApplicationRepository.kt
+++ src/main/kotlin/org/example/rootbe/domain/applications/domain/repository/VolunteerApplicationRepository.kt
@@ -14,4 +14,6 @@ interface VolunteerApplicationRepository : JpaRepository<VolunteerApplication, L
         user: User,
         volunteerPost: VolunteerPost,
     ): VolunteerApplication?
+
+    fun findAllByVolunteerPost(post: VolunteerPost): List<VolunteerApplication>
 }
