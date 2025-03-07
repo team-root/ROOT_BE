a593f8be8486719f35aee1cfb62bf916042b648e
coehgns <modoobiroo@gmail.com>
2025-03-07 14:27:52 +0900

    feat: (#88) GetAcceptedStudentService 추가

---
+++ src/main/kotlin/org/example/rootbe/domain/applications/service/GetAcceptedStudentsService.kt
@@ -0,0 +1,32 @@
+package org.example.rootbe.domain.applications.service
+
+import org.example.rootbe.domain.applications.domain.repository.VolunteerApplicationRepository
+import org.example.rootbe.domain.applications.presentation.dto.response.GetAcceptedStudentsResponse
+import org.example.rootbe.domain.post.facade.VolunteerPostFacade
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class GetAcceptedStudentsService(
+    private val postFacade: VolunteerPostFacade,
+    private val volunteerApplicationRepository: VolunteerApplicationRepository,
+) {
+    @Transactional(readOnly = true)
+    fun execute(postId: Long): GetAcceptedStudentsResponse {
+        val post = postFacade.getVolunteerPostById(postId)
+        val roles = post.roles
+        val applications = volunteerApplicationRepository.findAllByVolunteerPost(post)
+        val acceptedStudents = applications.filter { it.isAccepted }.map { it.user }
+
+        return GetAcceptedStudentsResponse(
+            acceptedStudents.map {
+                GetAcceptedStudentsResponse.AcceptedStudentsResponse(
+                    userId = it.id,
+                    name = it.name,
+                    grade = it.grade
+                )
+            },
+            roles.map { it.title }
+        )
+    }
+}
